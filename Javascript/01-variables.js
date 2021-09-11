a82696275759a4b9a5338c40bec9b607894d19de
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-10 23:43:06 -0500

    13 - Variables con var

---
+++ Javascript/01-variables.js
@@ -0,0 +1,13 @@
+//Ecmascript 2021 / ES12 / ES.NEXT
+//Variables var, let, const
+
+//Uso para variables globales
+var nombre = 'Brandon vargas';
+
+function mostrarNombre() {
+	var nombre = 'Juan';
+	console.log(nombre);
+}
+
+mostrarNombre();
+console.log(nombre);
