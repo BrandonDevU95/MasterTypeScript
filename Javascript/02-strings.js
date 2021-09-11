eb9787d5981f6cebbf7c7f7669e169a1dfcf7987
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-11 00:04:57 -0500

    16 - Template String

---
+++ Javascript/02-strings.js
@@ -0,0 +1,14 @@
+//Strings clasicos
+let nombre = 'Brandon';
+let apellido = 'Garcia';
+let nombreCompleto = nombre + ' ' + apellido;
+
+console.log(nomreCompleto);
+
+// Template strings
+let nombreCompleto2 = `${nombre} ${apellido}`;
+
+//Template strings html
+let nombreCompleto3 = `
+	<h1>Hola</h1>
+`;
