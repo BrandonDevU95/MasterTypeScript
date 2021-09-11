3177fcaa7582829627b419747f5cc73078f6eb49
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-11 00:13:03 -0500

    17 - Tipos de datos

---
+++ Javascript/03-tiposDatos.js
@@ -0,0 +1,41 @@
+//String
+let nombre = 'Juan';
+
+//Numbers
+let numero = 10;
+let decimal = 10.5;
+
+//Boolean
+let verdadero = true;
+let falso = false;
+let mayor = 0;
+let menor = 1;
+
+//Arrays
+let array = [1, 2, 3, 4, 5];
+let paises = ['España', 'Francia', 'Italia'];
+
+//JSON
+let persona = {
+	name = 'Juan',
+	age = 20,
+	country = 'España'
+};
+
+//Null
+let nulo = null;
+
+//Undefined
+let indefinido = undefined;
+
+console.log(
+	typeof nombre,
+	typeof numero,
+	typeof decimal,
+	typeof verdadero,
+	typeof falso,
+	typeof mayor,
+	typeof menor,
+	typeof array,
+	typeof paises
+);
