d827697d50d0fdee3b96019aa5dae109163737e9
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-10 23:51:45 -0500

    Variables con Let

--- Javascript/01-variables.js
+++ Javascript/01-variables.js
@@ -11,3 +11,38 @@ function mostrarNombre() {
 
 mostrarNombre();
 console.log(nombre);
+
+//Let
+//Hambíto de bloque Ahorro de memoria
+
+let pais = 'México';
+
+console.log(pais);
+
+function mostarPais() {
+	console.log(pais);
+}
+
+if (pais == 'México') {
+	let continete = 'America';
+	console.log(pais);
+}
+
+//No accesible fuera del bloque
+//console.log(continente);
+
+//Si no se declara el tipo de variable, se asigna por defecto var
+for (i = 0; i < 5; i++) {
+	console.log(i);
+}
+
+console.log(i);
+
+//Si espesificamos let, ya no es accesible desde fuera del bloque
+for (let i = 0; i < 5; i++) {
+	console.log(i);
+}
+
+console.log(i);
+
+mostarPais();
