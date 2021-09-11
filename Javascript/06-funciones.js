0c0874a98ea22670a2367abf2d3f8025139088cd
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-11 18:57:02 -0500

    26 - Funcion flecha

--- Javascript/06-funciones.js
+++ Javascript/06-funciones.js
@@ -12,3 +12,10 @@ function saludo2(persona = 'Juan') {
 }
 
 console.log(saludo2());
+
+//Funciones de flecha
+let saludo3 = (persona = 'Alberto') => 'Hola te mando un saludo ' + persona;
+console.log(saludo3());
+
+let saludo4 = (persona, pais) => 'Hola te mando un saludo ' + persona + ' de ' + pais;
+console.log(saludo4('Mario', 'España'));
