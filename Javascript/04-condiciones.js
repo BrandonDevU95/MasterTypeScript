ff570d61e0a5c4116b7d978bfaabf1480daedfd5
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-11 17:06:30 -0500

    22 - Condicion ternaria

---
+++ Javascript/04-condiciones.js
@@ -0,0 +1,17 @@
+//Operador ternario
+let persona = {
+	nombre: 'Juan',
+	edad: 20,
+};
+
+//Condicion classica
+if (persona.edad >= 18) {
+	console.log('Es mayor de edad');
+} else {
+	console.log('Es menor de edad');
+}
+
+//operador ternario
+console.log(persona.edad >= 18 ? 'Es mayor de edad' : 'Es menor de edad');
+
+//Diferencia entre == y === | != y !==
