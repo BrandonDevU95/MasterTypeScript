7fa2fd279f0ef9d7eaff890330b057fa03a7848f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-11 19:30:55 -0500

    30 - Spread

---
+++ Javascript/08-rest-spread.js
@@ -0,0 +1,22 @@
+let peliculas = ['Avengers', 'Spiderman', 'Batman'];
+
+console.log(peliculas);
+console.log(peliculas[0], peliculas[1], peliculas[2]);
+
+//Spread Operator
+console.log(...peliculas);
+
+function mostrarPeliculas(pelicula1, pelicula2) {
+	console.log(`
+		*** Mostrar Peliculas ***
+		Pelicula 1: ${pelicula1}
+		Pelicula 2: ${pelicula2}	
+	`);
+}
+
+mostrarPeliculas(...peliculas);
+
+let heroes = ['Spiderman', 'Capitan America'];
+let superheroes = ['Ironman', 'Capitan America', 'Thor', ...heroes];
+
+console.log(superheroes);
