ebec7d4937761b7ba75629aad275bbd44af3a4f2
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-11 20:09:32 -0500

    34 - Promesas P3

--- Javascript/09-promesas.js
+++ Javascript/09-promesas.js
@@ -19,15 +19,42 @@ let productos = [
 function getProductos() {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-			//resolve(productos);
-			reject(Error('No hay productos'));
+			resolve(productos);
+			//reject(Error('No hay productos'));
 		}, 500);
 	});
 }
 
+function getCompra() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve({
+				nombre: 'Monitor 27 pulgadas',
+				precio: 300,
+			});
+		}, 1000);
+	});
+}
+
+// getProductos()
+// 	.then((items) => {
+// 		console.log(items);
+// 	})
+// 	.catch((error) => {
+// 		console.log(error.message);
+// 	})
+// 	.finally(() => {
+// 		console.log('Finalizado');
+// 	});
+
+//Promesas en cadena
 getProductos()
 	.then((items) => {
-		console.log(items);
+		console.log('Productos: ' + items);
+		return getCompra;
+	})
+	.then((venta) => {
+		console.log('Producto comprado: ' + venta);
 	})
 	.catch((error) => {
 		console.log(error.message);
