a6d373f780b2a2a934d444373a7ea438f787320e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-13 20:39:29 -0500

    41 - Modulos P1

--- Javascript/12-async-await.js
+++ Javascript/12-async-await.js
@@ -20,16 +20,30 @@ function conseguirProductos() {
 	return new Promise((resolve, reject) => {
 		console.log('Cargando productos...');
 		setTimeout(() => {
-			// resolve(productos);
-			reject(new Error('Error al cargar los productos'));
-		}, 500);
+			resolve(productos);
+			// reject(new Error('Error al cargar los productos'));
+		}, 3000);
+	});
+}
+
+function conseguirNombres() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve('Brandon Vargas');
+			// reject(new Error('Error al cargar los productos'));
+		}, 3000);
 	});
 }
 
 async function getMisProductos() {
 	try {
-		let mis_productos = await conseguirProductos();
-		console.log(mis_productos);
+		console.log('Cargando datos...');
+		let informacion = await Promise.all([conseguirProductos(), conseguirNombres()]);
+		console.log(informacion);
+		// let mis_productos = await conseguirProductos();
+		// let mi_nombre = await conseguirNombres();
+		// console.log(mis_productos);
+		// console.log(mi_nombre);
 	} catch (error) {
 		console.log(error.message);
 	}
