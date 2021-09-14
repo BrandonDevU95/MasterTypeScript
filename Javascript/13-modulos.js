ce0dc9bd0f9584eb83565e7e96dc618fbc66988d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-13 20:54:30 -0500

    41 - Modulos P1

---
+++ Javascript/13-modulos.js
@@ -0,0 +1,4 @@
+import imprimirArray from './14-mi-modulo.js';
+
+console.log('Archivo main');
+imprimirArray(['a', 'b', 'c']);
---
+++ Javascript/14-mi-modulo.js
@@ -0,0 +1,9 @@
+function imprimirArray(array) {
+	array.forEach((element) => {
+		console.log(element);
+	});
+
+	return array;
+}
+
+export default imprimirArray;
