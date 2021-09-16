29886d34eb3a33a746763c04e0e1ffeff5884907
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-16 18:40:15 -0500

    48 - Visibilidad de propiedades

--- Javascript/17-clases.js
+++ Javascript/17-clases.js
@@ -3,6 +3,9 @@
 class Coche {
 	//propiedades y atributos
 
+	//Variable privada
+	#numeropuertas = 4;
+
 	constructor(
 		marca = 'Mercedez',
 		modelo = 'Clase A',
@@ -21,9 +24,14 @@ class Coche {
 	static saludar(nombre) {
 		return 'Hola ' + nombre;
 	}
+
+	get numeroPuertas() {
+		return this.#numeropuertas;
+	}
 }
 
 let mi_coche = new Coche();
 console.log(mi_coche.modelo);
 
 console.log(Coche.saludar('Juan'));
+console.log(mi_coche.numeroPuertas);
